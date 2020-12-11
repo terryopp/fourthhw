25e3063ed8dff93a15467f6a851d4a60a7ff4c70
TerryOPP <61347225+terryopp@users.noreply.github.com>
2020-12-11 10:44:40 +0400

    финальная версия

--- js/Calc.js
+++ js/Calc.js
@@ -1,8 +1,24 @@
 class Calc {
   constructor() {
-    this.first = Array;
-    this.second = Array;
+    this.first = [];
+    this.second = [];
   }
 
-  
+  creating(n) {
+    this.first = [];
+    this.second = [];
+    for (let i = 1; i <= n; i++) {
+      this.first.push(i);
+    }
+  }
+
+  random() {
+    return Math.ceil(Math.random()*(this.first.length - 1));
+  }
+
+  randomize() {
+    if (this.first.length == 0) return 0;
+    let n = this.random();
+    this.second.push(this.first.splice(n, 1));
+  }
 }
--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
 window.onload = function() {
-  
+  const calc = new Calc();
+
+  const n = document.getElementById('n');
+  const start = document.getElementById('start');
+  const answer = document.getElementById('answer');
+  const answer1 = document.getElementById('first');
+  const answer2 = document.getElementById('second');
+
+  function writting () {
+    answer1.innerHTML = calc.first.join(', ');
+    answer2.innerHTML = calc.second.join(', ');
+  }
+
+  n.addEventListener('keyup', () => {
+    if (n.value*1 > 1) calc.creating(n.value*1);
+    writting();
+  });
+
+  start.addEventListener('click', () => {
+    calc.randomize();
+    writting();
+  });
 }
